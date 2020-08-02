94c940c2f3ccf28c55494f34ef932d20c1866787
Marcelo Martins <dw.cv.mj@gmail.com>
2020-08-01 23:14:37 -0300

    Clean using

--- Source/Models/Candidate.cs
+++ Source/Models/Candidate.cs
@@ -1,10 +1,6 @@
-﻿using Codenation.Challenge.Models;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Codenation.Challenge.Models
 {
--- Source/Models/Challenge.cs
+++ Source/Models/Challenge.cs
@@ -1,11 +1,7 @@
-﻿using Codenation.Challenge.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Net.PeerToPeer.Collaboration;
-using System.Threading.Tasks;
 
 namespace Codenation.Challenge.Models
 {
